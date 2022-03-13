238f9c0a4fd08f04430f780acf732a619344ab90
Peterliang233 <ncuyanping666@126.com>
2022-03-13 21:43:15 +0800

    feat: 新增用户注册的接口

--- service/user/api/internal/logic/registrylogic.go
+++ service/user/api/internal/logic/registrylogic.go
@@ -1,9 +1,12 @@
 package logic
 
 import (
+	"book/common/errorx"
 	"book/service/user/api/internal/svc"
 	"book/service/user/api/internal/types"
+	"book/service/user/rpc/userclient"
 	"context"
+	"strings"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -23,7 +26,25 @@ func NewRegistryLogic(ctx context.Context, svcCtx *svc.ServiceContext) RegistryL
 }
 
 func (l *RegistryLogic) Registry(req types.RegistryReq) (resp *types.RegistryResp, err error) {
-	// todo: add your logic here and delete this line
+	if len(strings.TrimSpace(req.Password)) == 0 || len(strings.TrimSpace(req.Username)) == 0 ||
+		len(strings.TrimSpace(req.Gender)) == 0 || len(strings.TrimSpace(req.Number)) == 0 {
+		return nil, errorx.NewDefaultError("请求参数不能为空")
+	}
 
-	return
+	user := &userclient.RegistryReq{
+		Username: req.Username,
+		Password: req.Password,
+		Number:   req.Number,
+		Gender:   req.Gender,
+	}
+	userInfo, err := l.svcCtx.UserRpc.Registry(l.ctx, user)
+	if err != nil {
+		return nil, errorx.NewDefaultError("api注册失败")
+	}
+	return &types.RegistryResp{
+		Username: userInfo.GetUsername(),
+		Detail:   userInfo.GetDetail(),
+		Gender:   req.Gender,
+		Number:   req.Number,
+	}, nil
 }
